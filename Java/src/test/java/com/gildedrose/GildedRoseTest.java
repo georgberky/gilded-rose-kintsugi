6744a209e210ed3d161f6b183aabbf4941cafd37
Georg Berky <georg.berky@valtech.com>
2019-07-11 16:21:41 +0200

    Add tests in from phase 2
    https://github.com/signed/GildedRose-phase2

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,114 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	public void whenDayPasses_itemAgedBrie_shouldIncreaseQualityByOne()
+			throws Exception {
+		Item item = new Item(getAgedBrie(), notSellinDatePassed(), 10);
+		GildedRose app = createApp(item);
 
+		app.updateQuality();
+
+		assertThat(item.quality, is(11));
+	}
+
+	
+	@Test
+	public void whenDayPasses_normalItem_shouldDecreaseQualityByOne()
+			throws Exception {
+		Item item = new Item(anyName(), notSellinDatePassed(), 10);
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, is(9));
+	}
+
+	@Test
+	public void whenDayPasses_normalItem_shouldDecreaseDaysLeftByOne()
+			throws Exception {
+		Item item = new Item(anyName(), notSellinDatePassed(), 10);
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.sellIn, is(notSellinDatePassed() - 1));
+	}
+	
+	@Test
+	public void whenDayPasses_itemSulfuras__shouldNotChangeQuality() throws Exception {
+		Item item = new Item("Sulfuras, Hand of Ragnaros", notSellinDatePassed(), anyQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, is(anyQuality()));
+	}
+	
+	@Test
+	public void whenDayPasses_normalItemWithZeroQuality_shouldNotDecreaseQuality()
+			throws Exception {
+		Item item = new Item(anyName(), anySellingDate(), zeroQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, is(zeroQuality()));
+	}
+	
+	@Test
+	public void whenDayPasses_normalItemWithNegativeSellDate_shouldDecreaseQualityeByTwo()
+			throws Exception {
+		Item item = new Item(anyName(), anyNegativeSellingDate(), anyQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, is(anyQuality() -2));
+	}
+	
+	@Test
+	public void whenDayPasses_itemAgedBrieWithQualityFifty_shouldNotIncreaseQuality()
+			throws Exception {
+		Item item = new Item(getAgedBrie(), anySellingDate(), getMaxQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, is(getMaxQuality()));
+	}
+
+	private int anyNegativeSellingDate() {
+		return -1;
+	}
+
+
+	private int anyQuality() {
+		return 10;
+	}
+
+
+	private int getMaxQuality() {
+		return 50;
+	}
+
+	private int zeroQuality() {
+		return 0;
+	}
+
+	private int anySellingDate() {
+		return 0;
+	}
+
+	private GildedRose createApp(Item item) {
+		Item[] items = new Item[] { item };
+		GildedRose app = new GildedRose(items);
+		return app;
+	}
+
+	private String getAgedBrie() {
+		return "Aged Brie";
+	}
+
+	private int notSellinDatePassed() {
+		return 3;
+	}
+
+	private String anyName() {
+		return "foo";
+	}
 }
