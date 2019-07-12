9ae879134006f307450b6161dcafd736dba97352
Georg Berky <georg.berky@valtech.com>
2019-07-12 09:00:00 +0200

    Use decrease quality in all places

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,11 +19,11 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Aged Brie") &&
+                !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        //items[i].quality = items[i].quality - 1;
                         gildedItems[i].decreaseQuality();
                     }
                 }
@@ -56,7 +56,7 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                                gildedItems[i].decreaseQuality();
                             }
                         }
                     } else {
