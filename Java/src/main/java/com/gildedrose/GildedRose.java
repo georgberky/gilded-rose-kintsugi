270aadc3c036616dd32700b79563356ebe4c5a20
Georg Berky <georg.berky@posteo.de>
2020-01-11 13:12:54 +0100

    Simplify updateAgedBrie

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -69,15 +69,13 @@ class GildedRose {
 
     private void updateAgedBrie(Item item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality++;
         }
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn--;
 
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality++;
         }
     }
 }
