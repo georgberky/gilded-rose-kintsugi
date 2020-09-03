3aebfa89f8e8a09a530715708291b66316c94f34
Georg Berky <georg.berky@valtech.com>
2020-09-03 08:49:32 +0200

    add JUnit 5 and AssertJ

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,14 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-
-public class GildedRoseTest {
-
+class GildedRoseTest {
     @Test
-    public void foo() {
+    void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
-
 }
