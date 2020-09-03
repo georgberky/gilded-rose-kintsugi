28687b112a090b427f834e1a1b207ff11fc8bd87
Georg Berky <georg.berky@valtech.com>
2020-09-03 10:57:01 +0200

    add test for regular item quality

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,14 +1,37 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class GildedRoseTest {
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @ParameterizedTest
+    @MethodSource("qualityRegularItems")
+    void regularItems_lose1qualityPerDay_ifSellInNotPassed(int startQuality, int expectedQuality) {
+        //arrange
+        String name = "Regular Item";
+        int sellIn = 1;
+        Item[] items = new Item[] { new Item(name, sellIn, startQuality) };
         GildedRose app = new GildedRose(items);
+
+        //act
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        //assert
+        assertThat(items[0].quality).isEqualTo(expectedQuality);
+    }
+
+    private static Stream<Arguments> qualityRegularItems() {
+        return Stream.of(
+            arguments(1, 0),
+            arguments(2, 1),
+            arguments(3, 2)
+        );
     }
 }
